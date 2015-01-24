6a6f8c0a9ff8ddc64202689b7e4569139dbc959e
qwding <qwding@freewheel.tv>
2015-01-24 10:25:34 +0800

    time limit exceeded

---
+++ 1_two_sum/1_error.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+    	for(int i = numbers.size()-1; i >= 0 ; i--){
+        	int sub = target - numbers[i];
+            printf("sub num is %d\n",sub);
+        	if (sub > 0)
+        		for(int j = 0; j < i; j++){
+        			if (sub == numbers[j]){
+        				return {j+1,i+1};
+                    }
+        	}
+    	}
+        return {0,0};
+    }
+};
+
+
+int main()
+{
+    Solution solution;
+    vector<int> numbers = {1,3,2,11,5,15};
+    int target = 9;
+    vector<int> num = solution.twoSum(numbers,target);
+    cout << num[0] << endl;
+    cout << num[1] << endl;
+	return 0;
+}
